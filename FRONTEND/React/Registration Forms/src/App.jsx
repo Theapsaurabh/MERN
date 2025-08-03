4c8d50e53c872dcd51d2340dc2d15ebc8acc9a5b
Saurabh Pandey <ampsaurabh88877@gmail.com>
2025-08-03 10:01:06 +0530

    registration in React

---
+++ FRONTEND/React/Registration Forms/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RegistrationForm } from './Registration/Registration'
+
+const App = () => {
+  return (
+    <div>
+     <RegistrationForm/>
+    </div>
+  )
+}
+
+export default App
