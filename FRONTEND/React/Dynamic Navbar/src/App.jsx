e42d291385f9dd1427f86a61a92735f231fde78d
Saurabh Pandey <ampsaurabh88877@gmail.com>
2025-08-11 19:29:36 +0530

    navbar

---
+++ FRONTEND/React/Dynamic Navbar/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css"
+import { HeroSection } from "./components/HeroSection"
+import { Navbar } from "./components/Navbar"
+
+
+
+const App = () => {
+  return (
+    <>
+     <Navbar/>
+     <HeroSection/>
+    </>
+  )
+}
+
+export default App
